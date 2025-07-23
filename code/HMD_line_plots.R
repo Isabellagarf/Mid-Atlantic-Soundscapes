b05f10dd9b19a0939614249e3d4fe15dbe275278
Isabella Garfield <isabella.garfield@noaa.gov>
2025-07-23 16:32:51 -0400

    Updating code

--- code/HMD_line_plots.R
+++ code/HMD_line_plots.R
@@ -38,16 +38,15 @@ nrsData5 <- loadMultiscapeData(nrs11Folder5, timeBin = "1hour")
 #### --- joining deployments --- #####
 
 nrsData <- nrsData2 %>%
- full_join(nrsData3) #%>%
-# full_join(nrsData3) %>%
+ full_join(nrsData3) %>%
+ full_join(nrsData4) %>%
 # full_join(nrsData4) %>%
 # full_join(nrsData5) 
-
 
 #### suntools ####
 help(suntools)
 
-location <- matrix(c(-74.82, 38.71), nrow = 1)
+location <- matrix(c(-74.83, 37.68), nrow = 1)
 date_Time <- as.POSIXct("2023-09-16", tz = "UTC")
 
 Sunrise <- sunriset(crds = location, dateTime = date_Time, direction = "sunrise", POSIXct.out = TRUE)
@@ -115,7 +114,7 @@ astronomical_dusk_dechour <- astronomical_dusk %>%
 
 #### extract octave levels ####
 
-nrsData.oct <- createOctaveLevel(nrsData, type='ol') 
+nrsData.oct <- createOctaveLevel(nrsData4, type='ol') 
 
 # restructuring data to extract specific band during a specific month, ordered by hour of the day to analyze diel patterns #
 
@@ -124,59 +123,112 @@ nrsData.oct.mhd <- nrsData.oct %>%
   mutate(hour = as.numeric(hour)) %>%
   filter(month(UTC) == 9) %>% #adjust month
   mutate(day = day(UTC)) %>%
-  select(UTC, OL_500, day, hour) #adjust freq extracted
+  select(UTC, OL_2000, day, hour) #adjust freq extracted
 
 # calculating median, .1 and .9 quantile values for each hour #
 
 nrsData.medoct <- nrsData.oct.mhd %>%
   group_by(hour) %>%
+  na.omit(OL_2000) %>%
   summarise(
-    avg_ol = median(OL_500), #change freq if desired
-    q1_ol = quantile(OL_500, 0.1), #0.1 quantile SPL
-    q9_ol = quantile(OL_500, 0.9)  #0.9 quantile SPL
+    avg_ol = median(OL_2000), #change freq if desired
+    q1_ol = quantile(OL_2000, 0.1), #0.1 quantile SPL
+    q9_ol = quantile(OL_2000, 0.9)  #0.9 quantile SPL
   ) %>%
-  arrange(hour)
+  arrange(hour) 
 
-# basic plot with individual lines for every day of the month #
 
-plot400 <- ggplot(nrsData, aes(hour, HMD_400, group = day)) +
-  geom_line(aes(color=day,  alpha = 0.3)) +
-  theme_minimal()
 
-plot400
+# ------------- Adding multiple lines to one plot -------------#
 
-# summary plot with median and quantile values #
+# df 1 -------- #
+nrsData.oct2 <- createOctaveLevel(nrsData2, type='ol') 
 
-plot2000.med <- ggplot(nrsData.medoct, aes(x = hour, y = avg_ol)) +
-  geom_line(color = "black", size = 1) +
-  geom_point(color = "black") +
+# restructuring data to extract specific band during a specific month, ordered by hour of the day to analyze diel patterns #
+
+nrsData.oct.mhd2 <- nrsData.oct2 %>%
+  mutate(hour = substr(format(UTC,"%Y:%m:%d %H:%M:%S"), 12, 13)) %>% #lubridate doesn't recognize hour 00, so this is extracting the characters themselves
+  mutate(hour = as.numeric(hour)) %>%
+  filter(month(UTC) == 9) %>% #adjust month
+  mutate(day = day(UTC)) %>%
+  select(UTC, OL_2000, day, hour) #adjust freq extracted
+
+# calculating median, .1 and .9 quantile values for each hour #
+
+nrsData.medoct2 <- nrsData.oct.mhd2 %>%
+  group_by(hour) %>%
+  na.omit(OL_2000) %>%
+  summarise(
+    ES01 = median(OL_2000), #change freq if desired
+    q1_ol2 = quantile(OL_2000, 0.1), #0.1 quantile SPL
+    q9_ol2 = quantile(OL_2000, 0.9)  #0.9 quantile SPL
+  ) %>%
+  arrange(hour) 
+
+# df 2 -------- #
+nrsData.oct3 <- createOctaveLevel(nrsData3, type='ol') 
+
+# restructuring data to extract specific band during a specific month, ordered by hour of the day to analyze diel patterns #
+
+nrsData.oct.mhd3 <- nrsData.oct3 %>%
+  mutate(hour = substr(format(UTC,"%Y:%m:%d %H:%M:%S"), 12, 13)) %>% #lubridate doesn't recognize hour 00, so this is extracting the characters themselves
+  mutate(hour = as.numeric(hour)) %>%
+  filter(month(UTC) == 9) %>% #adjust month
+  mutate(day = day(UTC)) %>%
+  select(UTC, OL_2000, day, hour) #adjust freq extracted
+
+# calculating median, .1 and .9 quantile values for each hour #
+
+nrsData.medoct3 <- nrsData.oct.mhd3 %>%
+  group_by(hour) %>%
+  na.omit(OL_2000) %>%
+  summarise(
+    ES02 = median(OL_2000), #change freq if desired
+    q1_ol3 = quantile(OL_2000, 0.1), #0.1 quantile SPL
+    q9_ol3 = quantile(OL_2000, 0.9)  #0.9 quantile SPL
+  ) %>%
+  arrange(hour) 
+
+# df 3 -------- #
+nrsData.oct4 <- createOctaveLevel(nrsData4, type='ol') 
+
+# restructuring data to extract specific band during a specific month, ordered by hour of the day to analyze diel patterns #
+
+nrsData.oct.mhd4 <- nrsData.oct4 %>%
+  mutate(hour = substr(format(UTC,"%Y:%m:%d %H:%M:%S"), 12, 13)) %>% #lubridate doesn't recognize hour 00, so this is extracting the characters themselves
+  mutate(hour = as.numeric(hour)) %>%
+  filter(month(UTC) == 9) %>% #adjust month
+  mutate(day = day(UTC)) %>%
+  select(UTC, OL_2000, day, hour) #adjust freq extracted
+
+# calculating median, .1 and .9 quantile values for each hour #
+
+nrsData.medoct4 <- nrsData.oct.mhd4 %>%
+  group_by(hour) %>%
+  na.omit(OL_2000) %>%
+  summarise(
+    ES03 = median(OL_2000), #change freq if desired
+    q1_ol4 = quantile(OL_2000, 0.1), #0.1 quantile SPL
+    q9_ol4 = quantile(OL_2000, 0.9)  #0.9 quantile SPL
+  ) %>%
+  arrange(hour) 
+
+nrsData.medoct <- nrsData.medoct2 %>%
+  full_join(nrsData.medoct3) %>%
+  full_join(nrsData.medoct4)
+
+nrsData.medoct_long <- nrsData.medoct %>%
+  pivot_longer(cols = c(ES01, ES02, ES03),
+               names_to = "site",
+               values_to = "avg_ol")
+
+# --- sunset colors? line plot #
+plot2000.med <- ggplot(nrsData.medoct_long, aes(x = hour, y = avg_ol, group = site)) +
+  geom_line(aes(color = site), size = 1) +
+  #geom_point() +
   scale_x_continuous(limits = c(0, 24), breaks = seq(0, 22, by = 2)) +
-  geom_ribbon(aes(x = hour, ymin = q1_ol, ymax = q9_ol), alpha = 0.2, fill = "gray") +
-  geom_vline(xintercept = civil_dusk_dechour$decimal_hour, color = "gray40", linetype = "dashed") +
-  geom_vline(xintercept = nautical_dusk_dechour$decimal_hour, color = "gray20", linetype = "dashed") +
-  geom_vline(xintercept = astronomical_dusk_dechour$decimal_hour, color = "gray1", linetype = "dashed") +
-  geom_vline(xintercept = civil_dawn_dechour$decimal_hour, color = "gray40", linetype = "dashed") +
-  geom_vline(xintercept = nautical_dawn_dechour$decimal_hour, color = "gray20", linetype = "dashed") +
-  geom_vline(xintercept = astronomical_dawn_dechour$decimal_hour, color = "gray1", linetype = "dashed") +
-  geom_vline(xintercept = sunrise_dechour$decimal_hour, color = "gray60", linetype = "solid", width =  2) +
-  geom_vline(xintercept = sunset_dechour$decimal_hour, color = "gray60", linetype = "solid", width = 2) +
-  #scale_color_manual(name = "Key", values = c(Sunrise = "goldenrod3", Sunset = "darkmagenta")) +
-  #theme_minimal() +
-  labs(title="OL 2000: Diel fluctuations during September 2023 and 2024 at DB01", x="Hour", y = "dB")
-
-plot2000.med
-
-# save figure #
-
-ggsave(here("figs/OL_plots/DB01", "line500ol.sepDB01.png"), plot500.med, width= 7, height= 4, units= "in")
-
-
-# --- sunset colors? #
-plot500.med <- ggplot(nrsData.medoct, aes(x = hour, y = avg_ol)) +
-  geom_line(color = "black", size = 1) +
-  geom_point(color = "black") +
-  scale_x_continuous(limits = c(0, 24), breaks = seq(0, 22, by = 2)) +
-  geom_ribbon(aes(x = hour, ymin = q1_ol, ymax = q9_ol), alpha = 0.2, fill = "gray") +
+  scale_color_manual(values = c("ES01" = "gray20", "ES02" = "gray50", "ES03" = "gray80"))+
+  #geom_ribbon(aes(x = hour, ymin = q1_ol, ymax = q9_ol), alpha = 0.2, fill = "gray") +
   geom_vline(xintercept = civil_dusk_dechour$decimal_hour, color = "#F76218", linetype = "solid",size =  1, alpha = .5) +
   geom_vline(xintercept = nautical_dusk_dechour$decimal_hour, color = "#B10065", linetype = "solid",size =  .7, alpha = .5) +
   geom_vline(xintercept = astronomical_dusk_dechour$decimal_hour, color = "#740580", linetype = "solid",size =  .4, alpha = .5) +
@@ -186,7 +238,12 @@ plot500.med <- ggplot(nrsData.medoct, aes(x = hour, y = avg_ol)) +
   geom_vline(xintercept = sunrise_dechour$decimal_hour, color = "#F2C447", linetype = "solid", size =  2, alpha = .5) +
   geom_vline(xintercept = sunset_dechour$decimal_hour, color = "#F2C447", linetype = "solid", size = 2, alpha = .5) +
   theme_bw() +
-  labs(title="OL 500: Diel fluctuations during September 2023 and 2024 at DB01", x="Hour (UTC)", y = "dB")
+  labs(title="OL 2000: Diel fluctuations during September 2024 at ES sites", x="Hour (UTC)", y = "dB")
 
-plot500.med
+plot2000.med
+
+
+ggsave(here("figs/OL_plots", "line2000ol.sepES.all.png"), plot2000.med, width= 7, height= 4, units= "in")
+
+
 
