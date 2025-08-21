b53639887baf41da139f053a85f2d4430e82b2d8
anne-e-smith <anne.smith@noaa.gov>
2025-08-21 15:57:05 -0400

    updating deployment timelines figure

--- code/ST_Deployment_Timeline.R
+++ code/ST_Deployment_Timeline.R
@@ -1,26 +1,48 @@
-# Created by Bella Garfield, 7/9/25
+# Created by Bella Garfield, 7/9/25; edited by Annie Smith 8/21/2025
 library(ggplot2)
 library(ggalt)
 library(readxl)
+library(ggalt)
 
-
+devtools::install_github("hrbrmstr/ggalt")
 # Read in spreadsheet with deployment dates 
-Deployment_data <- read.csv("code/csv/Deployment_metadata_mid-Atl.csv")
+Deployment_data <- read.csv("data/Deployment_metadata_mid-Atl.csv")
 
 
 
 
-# option to set order of site names 
-Site_order <- as.character(unique(Deployment_data$SITE_NAME))
+# set order of site names 
+Deployment_data$SITE_NAME <- factor(Deployment_data$SITE_NAME, levels = c("DB01","DB02","DB03","ES01","ES02", "ES03", "CB01", "CB02","CB04", "CB03"))
 
 # order factor levels
 Status_order <- c("Processed Sucessfully","ERROR", "Omitted")
 
-# Number of factors to include in coloring of timeline 
-levels(factor(Deployment_data$DEPLOYMENT_STATUS)) # 9
+# plot - used by Annie
+deployment_timelines <- ggplot() +
+  geom_dumbbell(size=3, data = Deployment_data,
+                aes(y = factor(SITE_NAME),
+                    x = as.Date(START_USABLE_DATE_EST, format = "%m/%d/%Y"),
+                    xend = as.Date(END_USABLE_DATE_EST, format = "%m/%d/%Y"), 
+                    colour = factor(Status, level = Status_order)))+
+  scale_color_manual(name = "", values = c("Processed Sucessfully" = "aquamarine3",
+                                           "Omitted" ="slategray3",
+                                           "ERROR"= "#CC0000")) +
+  
+  ylab("SITE NAME") + xlab("DEPLOYMENT PERIOD (Month/Year)") + 
+  ggtitle("SoundTrap deployments included in Mid-Atlantic Soundscape Analysis")+
+  scale_x_date (date_breaks = "5 months", date_labels = "%m/%y")+
+  scale_y_discrete(limits=rev)+
+  theme(axis.text = element_text(size = 12), # format axis font 
+        axis.title.x = element_text(size = 12, face = "bold"),
+        axis.title.y = element_text(size = 12, face = "bold"),
+        text = element_text(size=12), 
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        legend.text=element_text(size=12)) 
 
+ggsave("figs/Deployment.timeline/plot.png", plot =deployment_timelines , width = 220, height = 120, units = "mm", dpi = 700)      
 
-
+plot(deployment_timelines)
 # define colours
 # myColours <- c('coral1','dodgerblue2','coral3','aquamarine3','dodgerblue4','lightsalmon2')
 # names(myColours) <- levels(factor(Deployment_data$DEPLOYMENT_STATUS))
